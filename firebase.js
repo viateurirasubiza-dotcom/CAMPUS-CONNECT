ba2bc5cf355a95b86017d09c66af10c80e6868dc
viateurirasubiza-dotcom <viateurirasubiza@gmail.com>
2026-06-04 00:12:57 -1000

    Add Firebase configuration and initialization

---
+++ firebase.js
@@ -0,0 +1,13 @@
+// Firebase config (uzasimbuza iyawe)
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
+};
+
+// Initialize Firebase
+// (Ushobora kongeramo Firebase CDN niba ukoresha real system)
+console.log("Firebase loaded");
